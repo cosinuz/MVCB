1e9e7a3ae3806e73dd38c3908b4e5df727d800b9
Nicolas Vignes <nicolas.vignes@ensimag.grenoble-inp.fr>
2014-04-04 12:25:18 +0200

    Ajout d'un script auto

---
+++ launch.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+echo -e "Lancement des serveurs"
+node server/server.js &
+sleep 3
+node signalingserver/server.js &
+
+echo -e "Serveur lancé"
+namevariable=$(uname)
+
+echo "$namevariable"
+
+if [ "$namevariable" = "Darwin" ]; then
+	echo -e "Lancement de Chrome sous OS X"
+	open /Applications/Google\ Chrome.app http://localhost:8001
+elif ["$namevariable" = "Linux"]; then
+	echo -e "Lancement de Chrome sous Linux"
+	chromium-browser http://localhost:8001
+
+fi
+echo -e "Fin du programme"
