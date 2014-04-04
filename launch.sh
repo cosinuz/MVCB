546242750e412b807205ee62a443f8a86f212b7c
Maxence Bouret <maxence.bouret@ensimag.fr>
2014-04-04 12:31:17 +0200

    Correction de launch.sh pour Linux

--- launch.sh
+++ launch.sh
@@ -13,9 +13,8 @@ echo "$namevariable"
 if [ "$namevariable" = "Darwin" ]; then
 	echo -e "Lancement de Chrome sous OS X"
 	open /Applications/Google\ Chrome.app http://localhost:8001
-elif ["$namevariable" = "Linux"]; then
-	echo -e "Lancement de Chrome sous Linux"
-	chromium-browser http://localhost:8001
-
+#elif [ "$namevariable" = "Linux" ]; then
+#	echo -e "Lancement de Chrome sous Linux"
+#    chromium-browser http://localhost:8001
 fi
 echo -e "Fin du programme"
