886a83948407233294d3bf2be6701f73f24e12e5
Nicolas Vignes <nico.vignes@gmail.com>
2014-04-18 03:56:35 -0400

    Ajout de la data et du drop pour la bd

--- server/mysql.js
+++ server/mysql.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
           host     : 'localhost',
-            user     : 'root',
-              password : ''
+            user     : 'vignesn',
+              password : 'darkknight23'
               });
 
 connection.connect();
