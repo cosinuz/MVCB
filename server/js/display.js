b0bd3d0199b62965b3a71ea3faf5e3f3da7dc37e
cosinuz <corentin.cos@gmail.com>
2014-04-25 10:01:10 +0200

    fix: dont remove current principal video if clicked on the corresponding mini video

--- server/js/display.js
+++ server/js/display.js
@@ -1,25 +1,41 @@
 var videoDisplayed = false;
 $(document).ready(function() {
 		$("#miniature").on("click",".miniature-video",function() {
-				        
-                        $(".principal-video").remove();
-				        new_video = $(this).clone();
-				        new_video.attr("id",new_video.attr("id")+"_principal");
-                        $("#principal").append(new_video);
-                        //no animation effect if a video is already displayed
-                        if(!videoDisplayed) {
-                            //to emphasize the animate effect
-                            new_video.css("width","0px");
-                            new_video.css("height","0px");
-                            $("#"+new_video.attr("id")).animate(
-                                {
+				var miniId = $(this).attr("id");
+                var principalId ="";                    
+                var newVideo;
+                //retrieving the principal id if possible
+                if((typeof $(".principal-video") !== "undefined") &&
+                    (typeof $(".principal-video").attr("id") !== "undefined")){
+                    principalId = $(".principal-video").attr("id").split("principalID")[0]; 
+                }
+                console.log("miniId : " + miniId);
+                console.log("principalId : " + principalId);
+                //if the given miniature video
+                //is not already displayed
+                if(miniId !== principalId) {
+                    $(".principal-video").remove();
+				    newVideo = $(this).clone();
+				    newVideo.attr("id",newVideo.attr("id")+"principalID");
+                    console.log("on devrait passer la");
+                    console.log(videoDisplayed);
+                    $("#principal").append(newVideo);
+                    //no animation effect if a video is already displayed
+                    if(!videoDisplayed) {
+                        //to emphasize the animate effect
+                        newVideo.css("width","0px");
+                        newVideo.css("height","0px");
+                        console.log(newVideo.attr("id"));
+                        $("#"+newVideo.attr("id")).animate(
+                            {
                                 "width": "+=400px",
                                 "height":"+=400px"
-                                },
-                                "slow");
-                        }
-    				    new_video.attr("class","principal-video");
-                        videoDisplayed = true;
+                            },
+                            "slow");
+                    }
+    			    newVideo.attr("class","principal-video");
+                    videoDisplayed = true;
+                }
 			});
 
 		$("#principal").on("click",".principal-video",function() {
